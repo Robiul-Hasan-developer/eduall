38beb5c1e5f34a20c7a78245dc3aae985c55771b
Robiul Hasan <robiulhasan9559@gmail.com>
2024-08-18 12:42:18 +0600

    EduAll HTML Template

---
+++ src/assets/js/main.js
@@ -0,0 +1,174 @@
+(function ($) {
+  "use strict";
+  
+  // ==========================================
+  //      Start Document Ready function
+  // ==========================================
+  $(document).ready(function () {
+    
+  // ============== Mobile Menu Sidebar & Offcanvas Js Start ========
+  $('.toggle-mobileMenu').on('click', function () {
+    $('.mobile-menu').addClass('active');
+    $('.side-overlay').addClass('show');
+    $('body').addClass('scroll-hide-sm');
+  }); 
+
+  $('.close-button, .side-overlay').on('click', function () {
+    $('.mobile-menu').removeClass('active');
+    $('.side-overlay').removeClass('show');
+    $('body').removeClass('scroll-hide-sm');
+  }); 
+  // ============== Mobile Menu Sidebar & Offcanvas Js End ========
+  
+  // ============== Mobile Nav Menu Dropdown Js Start =======================
+  var windowWidth = $(window).width(); 
+  
+  $('.has-submenu').on('click', function () {
+    var thisItem = $(this); 
+    
+    if(windowWidth < 992) {
+      if(thisItem.hasClass('active')) {
+        thisItem.removeClass('active')
+      } else {
+        $('.has-submenu').removeClass('active')
+        $(thisItem).addClass('active')
+      }
+      
+      var submenu = thisItem.find('.nav-submenu');
+      
+      $('.nav-submenu').not(submenu).slideUp(300);
+      submenu.slideToggle(300);
+    }
+    
+  });
+  // ============== Mobile Nav Menu Dropdown Js End =======================
+    
+  // ===================== Scroll Back to Top Js Start ======================
+  var progressPath = document.querySelector('.progress-wrap path');
+  var pathLength = progressPath.getTotalLength();
+  progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+  progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+  var updateProgress = function () {
+    var scroll = $(window).scrollTop();
+    var height = $(document).height() - $(window).height();
+    var progress = pathLength - (scroll * pathLength / height);
+    progressPath.style.strokeDashoffset = progress;
+  }
+  updateProgress();
+  $(window).scroll(updateProgress);
+  var offset = 50;
+  var duration = 550;
+  jQuery(window).on('scroll', function() {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery('.progress-wrap').addClass('active-progress');
+    } else {
+      jQuery('.progress-wrap').removeClass('active-progress');
+    }
+  });
+  jQuery('.progress-wrap').on('click', function(event) {
+    event.preventDefault();
+    jQuery('html, body').animate({scrollTop: 0}, duration);
+    return false;
+  })
+  // ===================== Scroll Back to Top Js End ======================
+
+  // ========================== add active class to ul>li top Active current page Js Start =====================
+  function dynamicActiveMenuClass(selector) {
+    let FileName = window.location.pathname.split("/").reverse()[0];
+
+    selector.find("li").each(function () {
+      let anchor = $(this).find("a");
+      if ($(anchor).attr("href") == FileName) {
+        $(this).addClass("activePage");
+      }
+    });
+    // if any li has activePage element add class
+    selector.children("li").each(function () {
+      if ($(this).find(".activePage").length) {
+        $(this).addClass("activePage");
+      }
+    });
+    // if no file name return
+    if ("" == FileName) {
+      selector.find("li").eq(0).addClass("activePage");
+    }
+  }
+  if ($('ul').length) {
+    dynamicActiveMenuClass($('ul'));
+  }
+  // ========================== add active class to ul>li top Active current page Js End =====================
+
+    
+  // ========================== Select2 Js Start =================================
+  $(document).ready(function() {
+    $('.js-example-basic-single').select2();
+  });
+  // ========================== Select2 Js End =================================
+  
+
+  
+  // ========================= Testimonial Four Slider Js Start ==============
+  // $('.testimonial-four-slider').slick({
+  //   slidesToShow: 2,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  //   speed: 1500,
+  //   dots: true,
+  //   pauseOnHover: true,
+  //   arrows: false,
+  //   draggable: true,
+  //   speed: 900,
+  //   infinite: true,
+  //   prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
+  //   nextArrow: '<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>',
+  //   responsive: [
+  //     {
+  //       breakpoint: 767,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         arrows: false,
+  //       }
+  //     }
+  //   ]
+  // });
+  // ========================= Testimonial Four Slider Js End ===================
+  
+  // ========================== Password Show Hide Js Start =====================
+  // $(".toggle-password").on('click', function() {
+  //   $(this).toggleClass(" la-eye-slash");
+  //   var input = $($(this).attr("id"));
+  //   if (input.attr("type") == "password") {
+  //   input.attr("type", "text");
+  //   } else {
+  //   input.attr("type", "password");
+  //   }
+  // });
+  // ========================== Password Show Hide Js End =====================
+
+  });
+  // ==========================================
+  //      End Document Ready function
+  // ==========================================
+
+  // ========================= Preloader Js Start =====================
+    $(window).on("load", function(){
+      $('.preloader').fadeOut(); 
+    })
+    // ========================= Preloader Js End=====================
+
+    // ========================= Header Sticky Js Start ==============
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() >= 260) {
+        $('.header').addClass('fixed-header');
+      }
+      else {
+          $('.header').removeClass('fixed-header');
+      }
+    }); 
+    // ========================= Header Sticky Js End===================
+
+})(jQuery);
